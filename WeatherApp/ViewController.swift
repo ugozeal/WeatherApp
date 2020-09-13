cefe643652776968b5928caf924aa0238af5a766
Ugozeal <okonkwodavidu@gmail.com>
2020-09-13 18:18:50 +0100

    UI Design- First commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by David U. Okonkwo on 9/13/20.
+//  Copyright © 2020 Decagon HQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+    }
+
+
+}
+
